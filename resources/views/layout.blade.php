11d5806f5a3bdaa02f5d468c1ba593c20b29dacb
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2021-01-28 19:27:42 -0500

    ajustes favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/img/main/logo.png">
+    <link rel="shortcut icon" href="/img/main/logo_dark.png">
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Big Bang Ideas Group SAS
